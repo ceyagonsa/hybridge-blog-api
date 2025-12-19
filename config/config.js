3ca76228aa3053a88a817acbe1a92d05d4ae34b9
Cesar Yahir Gonzalez <y4h1rr@gmail.com>
2025-12-19 14:11:19 -0600

    limpiar conexiones muertas cada segundo

--- config/config.js
+++ config/config.js
@@ -17,10 +17,11 @@ module.exports = {
       },
       // Configuración de Pool optimizada para Render + Supabase
       pool: {
-        max: 5,
-        min: 1,           // Mantiene al menos una conexión "viva"
-        acquire: 60000,   // Tiempo máximo de espera (60 segundos)
-        idle: 10000       // Cierra conexiones inactivas después de 10 segundos
+        max: 1,
+        min: 0,           // Mantiene al menos una conexión "viva"
+        acquire: 30000,   // Tiempo máximo de espera (60 segundos)
+        idle: 5000,
+        evict: 1000      // Cierra conexiones inactivas después de 10 segundos
       }
     }
   };
