e02889e1ce4e2acdbea81497cb78ec9b336f17d8
Cesar Yahir Gonzalez <y4h1rr@gmail.com>
2025-12-19 14:18:36 -0600

    Modificar el número de conexiones

--- config/config.js
+++ config/config.js
@@ -17,9 +17,9 @@ module.exports = {
       },
       // Configuración de Pool optimizada para Render + Supabase
       pool: {
-        max: 1,
+        max: 2,
         min: 0,           // Mantiene al menos una conexión "viva"
-        acquire: 30000,   // Tiempo máximo de espera (60 segundos)
+        acquire: 60000,   // Tiempo máximo de espera (60 segundos)
         idle: 5000,
         evict: 1000      // Cierra conexiones inactivas después de 10 segundos
       }
