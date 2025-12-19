401bc878aeed0eb09c9c7159018ab5d87abf84c3
Cesar Yahir Gonzalez <y4h1rr@gmail.com>
2025-12-19 13:34:07 -0600

    Fix: pool added to config

--- config/config.js
+++ config/config.js
@@ -1,4 +1,11 @@
 module.exports = {
+    development: {
+      username: "postgres",
+      password: "password",
+      database: "database_dev",
+      host: "127.0.0.1",
+      dialect: "postgres"
+    },
     production: {
       use_env_variable: 'DATABASE_URL',
       dialect: 'postgres',
@@ -8,11 +15,11 @@ module.exports = {
           rejectUnauthorized: false
         }
       },
-      // Agrega esto para que la conexión no se quede colgada:
+      // El 'pool' es vital para que la conexión no se quede colgada
       pool: {
         max: 5,
         min: 0,
-        acquire: 30000,
+        acquire: 30000, 
         idle: 10000
       }
     }
