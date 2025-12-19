2d6b591a47d7f26e5748fd66680e61074f331114
Cesar Yahir Gonzalez <y4h1rr@gmail.com>
2025-12-19 11:35:25 -0600

    Fix final: Host 0.0.0.0 y puerto para Render

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const db = require('./models');
 const authorsRoutes = require('./routes/authors.routes');
-const postsRoutes = require('./routes/posts.routes'); // 1. Importar posts
+const postsRoutes = require('./routes/posts.routes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000; // Render usa el 10000 por defecto
 
 app.use(express.json());
 
-// Registrar rutas
 app.use('/api/authors', authorsRoutes);
-app.use('/api/posts', postsRoutes); // 2. Usar posts
+app.use('/api/posts', postsRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Bienvenido a la API de Hybridge Blog' });
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
