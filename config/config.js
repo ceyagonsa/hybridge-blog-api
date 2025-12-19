9ba2bd72bebe1718bd56d60fb540c1ab3247cc6c
Cesar Yahir Gonzalez <y4h1rr@gmail.com>
2025-12-19 12:06:48 -0600

    ya funciona por favor

--- config/config.js
+++ config/config.js
@@ -1,13 +1,6 @@
 module.exports = {
-    development: {
-      username: "postgres",
-      password: "password",
-      database: "database_dev",
-      host: "127.0.0.1",
-      dialect: "postgres"
-    },
     production: {
-      url: process.env.DATABASE_URL, // Cambiamos 'use_env_variable' por 'url'
+      use_env_variable: 'DATABASE_URL',
       dialect: 'postgres',
       dialectOptions: {
         ssl: {
