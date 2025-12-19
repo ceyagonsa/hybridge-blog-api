ddffff8d4167fb27bed0f01e362bd07bd42a5b38
Cesar Yahir Gonzalez <y4h1rr@gmail.com>
2025-12-19 14:42:23 -0600

    nada

--- config/config.js
+++ config/config.js
@@ -17,11 +17,11 @@ module.exports = {
         keepAlive: true
       },
       pool: {
-        max: 1,            // Estricto: 1 sola conexión para evitar saturación
+        max: 3,            // Estricto: 1 sola conexión para evitar saturación
         min: 0,
-        acquire: 60000,    // 60 segundos de paciencia para conectar
-        idle: 1000,        // Libera la conexión en 1 segundo
-        evict: 500         // Limpieza constante de conexiones muertas
+        acquire: 30000,    // 60 segundos de paciencia para conectar
+        idle: 10000,        // Libera la conexión en 1 segundo
+        // evict: 500         // Limpieza constante de conexiones muertas
       }
     }
   };
